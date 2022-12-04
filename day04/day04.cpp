32e8e9ba6906b7e832eb048d3dc0aa61222d6be8
David Bång <dadda121@gmail.com>
2022-12-04 11:37:58 +0100

    day04, cleanup

--- day04/day04.cpp
+++ day04/day04.cpp
@@ -1,6 +1,7 @@
 #include "../common.h"
 #include <iostream>
 
+
 int main()
 {
     std::vector<std::string> lines = GetLinesInFile("input.txt");
@@ -11,15 +12,20 @@ int main()
         std::vector<std::string> sections = GetWords(line, ',');
         auto elf1_timeslots = GetWords(sections[0], '-');
         auto elf2_timeslots = GetWords(sections[1], '-');
-        auto elf1_start_time = std::stoi(elf1_timeslots[0]);
-        auto elf2_start_time = std::stoi(elf2_timeslots[0]);
-        auto elf1_end_time = std::stoi(elf1_timeslots[1]);
-        auto elf2_end_time = std::stoi(elf2_timeslots[1]);
+        auto start1 = std::stoi(elf1_timeslots[0]);
+        auto start2 = std::stoi(elf2_timeslots[0]);
+        auto end1 = std::stoi(elf1_timeslots[1]);
+        auto end2 = std::stoi(elf2_timeslots[1]);
 
-        if ((elf1_start_time <= elf2_start_time && elf1_end_time >= elf2_end_time) || (elf2_start_time <= elf1_start_time && elf2_end_time >= elf1_end_time))
+        auto inside_range = [](auto low_num, auto high_num, auto low_range, auto high_range)
+        {
+            return low_range <= low_num && high_range >= high_num;
+        };
+
+        if (inside_range(start2, end2, start1, end1) || inside_range(start1, end1, start2, end2))
             part1_overlap_count++;
 
-        if ((elf1_start_time <= elf2_start_time && elf1_end_time >= elf2_start_time) || (elf2_start_time <= elf1_start_time && elf2_end_time >= elf1_start_time))
+        if (inside_range(start2, start2, start1, end1) || inside_range(start1, start1, start2, end2))
             part2_overlap_count++;
 
     }
