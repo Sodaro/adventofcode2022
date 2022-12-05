ab26195025806a8eb8a7a11ef14a2e6f130b0d61
David Bång <dadda121@gmail.com>
2022-12-05 10:21:52 +0100

    day05 cleaned up

--- day05/day05.cpp
+++ day05/day05.cpp
@@ -1,81 +1,60 @@
 #include "../common.h"
 #include <iostream>
-#include <deque>
 int main()
 {
     std::vector<std::string> lines = GetLinesInFile("input.txt");
-    std::vector<std::deque<char>> part1_crates;
-    std::vector<std::deque<char>> part2_crates;
-
-    for (int i = 0; i < lines[0].size() / 3; i++)
+    std::vector<std::string> part1_crates{"", "", "", "", "", "", "", "", ""};
+    int i;
+    for (i = 0; i < lines.size(); i++)
     {
-        part1_crates.emplace_back(std::deque<char>());
-        part2_crates.emplace_back(std::deque<char>());
-    }
-
-    bool reading_moves = false;
-    for (const auto& line : lines)
-    {
+        auto& line = lines[i];
         if (line.size() == 0 || line[1] == '1')
-        {
-            reading_moves = true;
-            continue;
-        }
-        if (!reading_moves)
-        {
-            for (int i = 1; i < line.size(); i += 4)
-            {
-                if (line[i] == ' ')
-                    continue;
+            break;
 
-                int index = i / 4;
-                part1_crates[index].emplace_back(line[i]);
-                part2_crates[index].emplace_back(line[i]);
-            }
-        }
-        else
+        for (int j = 1; j < line.size(); j += 4)
         {
-            auto params = GetWords(line, ' ');
-            int count = std::stoi(params[1]);
-            int start_index = std::stoi(params[3])-1;
-            int target_index = std::stoi(params[5])-1;
+            if (line[j] == ' ')
+                continue;
 
-            std::string chunk = "";
-            for (int i = 0; i < count; i++)
-            {
-                auto crate = part1_crates[start_index].front();
-                part1_crates[target_index].emplace_front(crate);
-                part1_crates[start_index].pop_front();
-                
-                crate = part2_crates[start_index].front();
-                part2_crates[start_index].pop_front();
-                chunk.push_back(crate);
-            }
-            for (int i = chunk.size()-1; i >= 0; i--)
-            {
-                auto crate = chunk[i];
-                part2_crates[target_index].emplace_front(crate);
-            }
+            part1_crates[j / 4].push_back(line[j]);
         }
     }
-    std::string result1 = "";
-    
-    for (int i = 0; i < part1_crates.size(); i++)
+    std::vector<std::string> part2_crates(part1_crates.begin(), part1_crates.end());
+    for (i = i+2; i < lines.size(); i++)
     {
-        if (part1_crates[i].empty())
-            continue;
+        auto& line = lines[i];
+        auto params = GetWords(line, ' ');
+        int count = std::stoi(params[1]);
+        int start_index = std::stoi(params[3]) - 1;
+        int target_index = std::stoi(params[5]) - 1;
 
-        result1.push_back(part1_crates[i].front());
+        for (int j = 0; j < count; j++)
+        {
+            auto& start = part1_crates[start_index];
+            auto& target = part1_crates[target_index];
+            target.insert(target.begin(), start[0]);
+            start.erase(0, 1);
+        }
+
+        auto& start = part2_crates[start_index];
+        std::string chunk = start.substr(0, count);
+        start.erase(0, count);
+        auto& target = part2_crates[target_index];
+        target.insert(0, chunk);
     }
-    std::cout << result1 << std::endl;
 
-    std::string result2 = "";
-    for (int i = 0; i < part2_crates.size(); i++)
+    auto print_result = [](const std::vector<std::string>& vec)
     {
-        if (part2_crates[i].empty())
-            continue;
+        std::string result = "";
+        for (int i = 0; i < vec.size(); i++)
+        {
+            if (vec[i].empty())
+                continue;
 
-        result2.push_back(part2_crates[i].front());
-    }
-    std::cout << result2 << std::endl;
+            result.push_back(vec[i].front());
+        }
+        std::cout << result << std::endl;
+    };
+    print_result(part1_crates);
+    print_result(part2_crates);
 }
